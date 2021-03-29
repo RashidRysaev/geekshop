75f3edd13ebd9f791730f5b91342bf5dab4dbcd4
Rashid Rysaev <64075042+RashidRysaev@users.noreply.github.com>
2021-03-29 02:22:17 +0200

    Lesson 14 (#7)
    * lesson_14. debug-tool-bar
    
    * lesson_14. Testing tools
    
    * lesson_14. Load testing
    
    * lesson_14. Query renew
    
    * lesson_14. DB twiks

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -12,10 +12,13 @@ urlpatterns = [
     path("", include("social_django.urls", namespace="social")),
     re_path(r"^auth/", include("authnapp.urls", namespace="auth")),
     re_path(r"^basket/", include("basketapp.urls", namespace="basket")),
-    re_path(r"^admin/", include("adminapp.urls", namespace="admin")),
     re_path(r"^order/", include("ordersapp.urls", namespace="order")),
+    re_path(r"^admin/", include("adminapp.urls", namespace="admin")),
 ]
 
 
 if settings.DEBUG:
+    import debug_toolbar
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [re_path(r"^__debug__/", include(debug_toolbar.urls))]
