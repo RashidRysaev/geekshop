1c1c06ce1c1661e139b776a96450b897d6efbabb
Rashid Rysaev <64075042+RashidRysaev@users.noreply.github.com>
2021-03-30 14:50:06 +0200

    Lesson_15 (#8)
    * Settings setup
    
    * lesson_15. Basket cached methods
    
    * lesson_15. Low level cache
    
    * lesson_15. Template cache
    
    * lesson_15. View cache
    
    * lesson_15. Cache entire site
    
    * lesson_15. Screenshots added, code cleaned up
    
    Co-authored-by: Django <django@194-67-112-77.cloudvps.regruhosting.ru>

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    # "django.middleware.csrf.CsrfViewMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -306,3 +306,18 @@ if DEBUG:
         "debug_toolbar.panels.profiling.ProfilingPanel",
         "template_profiler_panel.panels.template.TemplateProfilerPanel",
     ]
+
+CACHE_MIDDLEWARE_ALIAS = "default"
+CACHE_MIDDLEWARE_SECONDS = 120
+CACHE_MIDDLEWARE_KEY_PREFIX = "geekbrains"
+
+# Be carefull if you have Windows! Install Memcached before run project!
+#     https://www.ubergizmo.com/how-to/install-memcached-windows/
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.memcached.MemcachedCache",
+        "LOCATION": "127.0.0.1:11211",
+    }
+}
+
+LOW_CACHE = True
